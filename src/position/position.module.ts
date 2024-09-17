b4009c9fb9adf8eea82a2dffab7a517ae217d282
Felix Nguyen <phuc.nguyenquang@orientsoftware.com>
2024-09-18 02:39:20 +0700

    remove code

--- src/position/position.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PositionController } from './position.controller';
-
-@Module({
-  controllers: [PositionController],
-})
-export class PositionModule {}
